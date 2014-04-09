1144eb1839d742f2db2d227c7a153bce50d6c8a3
paerhati <faruh.paerhati@gmail.com>
2014-04-09 13:29:36 -0400

    Finished the content page to be functional

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
 // JavaScript For the Index Page
+
+// JavaScript for updating and managing the navigation throughout the lesson.
+
+$(function() {
+    
+    $('#next-lesson-button').on('click', function(){
+        var current = $('#navigation .current');
+        var currentContent = $('#content-panel .current');
+        var next = $(current).next();
+        var nextContent = $(currentContent).next();
+        
+        // If we are at the last lesson point
+        if(!next.next().prop('tagName')){
+            // Switch the button to a handshake button.
+            
+            $('#next-lesson-button').hide();
+            $('#handshake-button').show();
+        }
+        
+        current.toggleClass('current');
+        next.toggleClass('current'); 
+        
+        currentContent.toggleClass('current');
+        nextContent.toggleClass('current');
+        
+    });
+    
+});

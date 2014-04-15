e1e34ceb2d458da1d0197226ac4e408ca8366b5f
paerhati <faruh.paerhati@gmail.com>
2014-04-15 12:44:03 -0400

    Made the navigation task bar scrollable, and auto scroll to keep the current task point in view. Also added all 14 lesson points text

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,35 @@
 
 $(function() {
     
-    $('#next-lesson-button').on('click', function(){
+    // Scrolling the navigation bar to a certain task
+    function scrollTo(taskNum){
+        $('#navigation').animate({
+          scrollLeft: 220*(taskNum - 1)
+        }, 250);
+    }
+    
+    // Shifting to a certain task point
+    function goToTask(taskNum){
         var current = $('#navigation .current');
         var currentContent = $('#content-panel .current');
-        var next = $(current).next();
-        var nextContent = $(currentContent).next();
         
-        // If we are at the last lesson point
+        var next = $($('#navigation li').get(taskNum - 1));
+        var nextContent = $($('#content-panel .lesson').get(taskNum - 1));
+        
+        console.log(nextContent);
+        
+        // If we are going to the last lesson point
         if(!next.next().prop('tagName')){
             // Switch the button to a handshake button.
-            
             $('#next-lesson-button').hide();
             $('#handshake-button').show();
+        }
+        
+        // If we are going from the last point to a previous point...
+        if(!current.next().prop('tagName')){
+            // Switch the button to a handshake button.
+            $('#handshake-button').hide();
+            $('#next-lesson-button').show();
         }
         
         current.toggleClass('current');
@@ -23,7 +40,26 @@ $(function() {
         
         currentContent.toggleClass('current');
         nextContent.toggleClass('current');
+    }
+    
+    // Advancing to the next lesson point
+    $('#next-lesson-button').on('click', function(){
         
+        var nextTaskNum = $('#navigation li.current').index() + 1
+        
+        goToTask(nextTaskNum);
+        
+        // Lastly, scroll the navigation so that the point stays in view of the user.
+        
+        scrollTo(nextTaskNum);
+    });
+    
+    // Going to a specific lesson point
+    $('#navigation li:not(".current") .fa').on('click', function(event){
+        console.log($(event.target).parent().parent().index());
+        
+        var taskNum = $(event.target).parent().parent().index();
+        goToTask(taskNum);
     });
     
     $('#handshake-button').on('click', function(){
@@ -33,6 +69,8 @@ $(function() {
     $('#return-to-board-button').on('click', function(){
         $('.completion-container').hide();
     });
+  
+    // Making the webcam feeds dynamic
 
     $('#video-1').on('click', function() {
         $('.video-capture-max').attr("src", "static/google-hangout-1.png");
@@ -65,3 +103,4 @@ $(function() {
     });
 
 });
+

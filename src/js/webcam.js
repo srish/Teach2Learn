abe251ef3a85c81c10380dbabab3dd10c33b50cd
brau0300 <marcusbraun@gmail.com>
2014-05-07 15:53:35 -0400

    Add mute/pause buttons

--- src/js/webcam.js
+++ src/js/webcam.js
@@ -1,5 +1,7 @@
 // used to hold the interval timer that updates the focused video
 var TIMER;
+var IS_MUTED = false;
+var IS_VIDEO_OFF = false;
 var localStream;
 var focusedStream;
 var context;
@@ -9,7 +11,7 @@ $(function() {
         // the id/element dom element that will hold "our" video
         localVideoEl: 'localStream',
         // the id/element dom element that will hold remote videos
-        remoteVideosEl: 'video-chooser',
+        remoteVideosEl: 'thumbnails',
         // immediately ask for camera access
         autoRequestMedia: true
     }); 
@@ -17,6 +19,7 @@ $(function() {
     focusedStream = document.getElementById("focusedStream");
     context = focusedStream.getContext('2d');
     TIMER = setInterval(function() {capture(localStream, true);}, 42);
+    $(localStream).css('cursor', 'pointer');
     // when local video clicked toggle focus
     $(localStream).click(function() {
         clearInterval(TIMER);
@@ -25,6 +28,7 @@ $(function() {
     // when videos are added, set click listeners
     webrtc.on('videoAdded', function (video, peer) {
         $(video).attr('width', '100%');
+        $(video).css('cursor', 'pointer');
         $(video).click(function() {
             clearInterval(TIMER);
             TIMER = setInterval(function() {capture(video, false);}, 42);
@@ -35,8 +39,50 @@ $(function() {
         // you can name it anything
         webrtc.joinRoom('teach2learn');
     });
+    $('.video').click(function (e) {
+        var $target = $('.btn.video');
+        if (IS_VIDEO_OFF) {
+            webrtc.resumeVideo();
+        } else {
+            webrtc.pauseVideo();
+        }
+        IS_VIDEO_OFF = !IS_VIDEO_OFF;
+        var cls = getStateIcon('video', IS_VIDEO_OFF);
+        $target.empty();
+        $target.append('<i class="fa ' + cls + ' video"></i>');
+    });
+    $('.mute').click(function (e) {
+        var $target = $('.btn.mute');
+        if (IS_MUTED) {
+            webrtc.unmute();
+        } else {
+            webrtc.mute();
+        }
+        IS_MUTED = !IS_MUTED;
+        var cls = getStateIcon('mute', IS_MUTED);
+        $target.empty();
+        $target.append('<i class="fa ' + cls + ' mute"></i>');
+    });
 });
 
+/*
+ * Helper function for getting the appropriate icon for webcam control hovers
+ */
+function getStateIcon(feature, state) {
+    if (feature == 'video') {
+        if (state) { // is video off?
+            return 'fa-eye';
+        } else {
+            return 'fa-eye-slash';
+        }
+    } else if (feature == 'mute') {
+        if (state) { // is currently muted?
+            return 'fa-microphone';
+        } else {
+            return 'fa-microphone-slash';
+        }
+    }
+}
 
 /*
  * Function that updates a cavnas with a still frame of the video
